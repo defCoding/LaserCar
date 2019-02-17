45162993d5f558e65e9493bde45e6cd2a9027ab0
Kevin Cao <kcao1998@gmail.com>
2019-02-17 03:28:41 -0500

    Created movable laser.

---
+++ Laser/frmLaser.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Laser
+{
+    partial class frmLaser
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.srlPort = new System.IO.Ports.SerialPort(this.components);
+            this.lblCoordinates = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // srlPort
+            // 
+            this.srlPort.PortName = "COM4";
+            // 
+            // lblCoordinates
+            // 
+            this.lblCoordinates.AutoSize = true;
+            this.lblCoordinates.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblCoordinates.Location = new System.Drawing.Point(8, 8);
+            this.lblCoordinates.Name = "lblCoordinates";
+            this.lblCoordinates.Size = new System.Drawing.Size(60, 19);
+            this.lblCoordinates.TabIndex = 0;
+            this.lblCoordinates.Text = "X:   Y:   ";
+            // 
+            // frmLaser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 953);
+            this.Controls.Add(this.lblCoordinates);
+            this.Name = "frmLaser";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Laser Control";
+            this.Load += new System.EventHandler(this.frmLaser_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.frmLaser_KeyPress);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.frmLaser_MouseMove);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort srlPort;
+        private System.Windows.Forms.Label lblCoordinates;
+    }
+}
+
---
+++ Laser/frmLaser.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+
+namespace Laser
+{
+    public partial class frmLaser : Form
+    {
+        public Stopwatch watch { get; set; }
+
+        public frmLaser()
+        {
+            InitializeComponent();
+        }
+
+        private void frmLaser_Load(object sender, EventArgs e)
+        {
+            this.watch = Stopwatch.StartNew();
+            this.srlPort.Open();
+        }
+
+        private void frmLaser_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.lblCoordinates.Text = "X: " + e.X + " Y: " + e.Y;
+            writeToPort(new Point(e.X, e.Y));
+        }
+
+        public void writeToPort(Point coordinates)
+        {
+            //this.watch.ElapsedMilliseconds > 15 
+            if (this.watch.ElapsedMilliseconds > 14 && this.srlPort.IsOpen)
+            {
+                this.srlPort.Write(String.Format("X{0}Y{1}",
+                    (180 - (coordinates.X / (this.Size.Width / 180))),
+                    (180 -(coordinates.Y / (this.Size.Height / 180)))));
+
+                this.watch = Stopwatch.StartNew();
+            }
+        }
+
+        private void frmLaser_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 'p')
+            {
+                if (this.srlPort.IsOpen) {
+                    this.srlPort.Close();
+                } else
+                {
+                    this.srlPort.Open();
+                }
+            } else if (e.KeyChar == 'r')
+            {
+                this.srlPort.Write(String.Format("X{0}Y{1}",
+                    (180 - (384 / (this.Size.Width / 180))),
+                    (180 - (683 / (this.Size.Height / 180)))));
+            } else
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
